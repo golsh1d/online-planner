2451e87db2a24b56e02e86d5234567fddb282cd9
golshid <golsheedd@gmail.com>
2025-04-06 10:52:20 +0330

    finish nav bar for great screens

--- src/Components/navbar/NavBar.js
+++ src/Components/navbar/NavBar.js
@@ -11,34 +11,49 @@ export default function NavBar() {
   const [isLogin, setIsLogin] = useState();
 
   useEffect(() => {
-    setIsLogin(true);
+    setIsLogin(false);
   }, []);
 
   return (
-    <nav className="NavBar-container" dir="rtl">
+    <nav
+      className="NavBar-container NavBar-container-max-768 NavBar-container-min-768"
+      dir="rtl"
+    >
       <div className="NavBar-right">
-        <div className="NavBar-img-container">
+        <div className="NavBar-img-container NavBar-img-container-min-768 NavBar-img-container-min-1024 NavBar-img-container-min-1280 NavBar-img-container-min-1920">
           <img src="./Img/logo.jpg" alt="logo" />
         </div>
         <ul className="NavBar-menu">
-          <li className="NavBar-list-item">
-            <Link className="NavBar-links" to="/">
+          <li className="NavBar-list-item-min-1920 NavBar-list-item-min-1280 NavBar-list-item-min-1024 NavBar-list-item-min-768">
+            <Link
+              className="NavBar-links NavBar-links-min-1920 NavBar-links-min-1280 NavBar-links-min-1024 NavBar-links-min-768"
+              to="/"
+            >
               صفحه اصلی
             </Link>
           </li>
-          <li className="NavBar-list-item">
-            <Link className="NavBar-links" to="/product">
+          <li className="NavBar-list-item-min-1920 NavBar-list-item-min-1280 NavBar-list-item-min-1024 NavBar-list-item-min-768">
+            <Link
+              className="NavBar-links NavBar-links-min-1920 NavBar-links-min-1280 NavBar-links-min-1024 NavBar-links-min-768"
+              to="/product"
+            >
               محصولات
             </Link>
           </li>
-          <li className="NavBar-list-item">
-            <Link className="NavBar-links" to="/usage">
+          <li className="NavBar-list-item-min-1920 NavBar-list-item-min-1280 NavBar-list-item-min-1024 NavBar-list-item-min-768">
+            <Link
+              className="NavBar-links NavBar-links-min-1920 NavBar-links-min-1280 NavBar-links-min-1024 NavBar-links-min-768"
+              to="/usage"
+            >
               آموزش
             </Link>
           </li>
           {isLogin && (
-            <li className="NavBar-list-item">
-              <Link className="NavBar-links" to="/onlinePlanner">
+            <li className="NavBar-list-item-min-1920 NavBar-list-item-min-1280 NavBar-list-item-min-1024 NavBar-list-item-min-768">
+              <Link
+                className="NavBar-links NavBar-links-min-1920 NavBar-links-min-1280 NavBar-links-min-1024 NavBar-links-min-768"
+                to="/onlinePlanner"
+              >
                 برنامه
               </Link>
             </li>
@@ -49,26 +64,20 @@ export default function NavBar() {
         {isLogin ? (
           <>
             <div className="NavBar-logout-container">
-              <PersonIcon
-                style={{ fontSize: "40px" }}
-                className="NavBar-user-icon"
-              />
-              <div className="NavBar-logout">
-                <LogoutIcon
-                  style={{ fontSize: "20px" }}
-                  className="NavBar-logout-icon"
-                />
+              <PersonIcon className="NavBar-user-icon NavBar-user-icon-min-1280 NavBar-user-icon-min-768" />
+              <div className="NavBar-logout NavBar-logout-min-1280 NavBar-logout-min-768">
+                <LogoutIcon className="NavBar-logout-icon NavBar-logout-icon-min-1280 NavBar-logout-icon-min-768" />
                 <span>خروج</span>
               </div>
             </div>
           </>
         ) : (
-          <Link to="/signUp" className="NavBar-links NavBar-signup-container">
+          <Link
+            to="/signUp"
+            className="NavBar-links NavBar-links-min-1920 NavBar-links-min-1280 NavBar-links-min-1024 NavBar-links-min-768 NavBar-signup-container"
+          >
             <span>ثبت نام</span>
-            <LoginIcon
-              style={{ fontSize: "30px" }}
-              className="NavBar-login-icon"
-            />
+            <LoginIcon className="NavBar-login-icon NavBar-login-icon-min-1920 NavBar-login-icon-min-1280 NavBar-login-icon-min-1024 NavBar-login-icon-min-768" />
           </Link>
         )}
       </div>
