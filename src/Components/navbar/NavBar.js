0ae78e36a484b74a50d9672fea7d16fb1620f0dc
golshid <golsheedd@gmail.com>
2025-04-06 08:06:18 +0330

    finish nav bar for 1920px

--- src/Components/navbar/NavBar.js
+++ src/Components/navbar/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+// icons
 import LoginIcon from "@mui/icons-material/Login";
+import PersonIcon from "@mui/icons-material/Person";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export default function NavBar() {
   const [isLogin, setIsLogin] = useState();
@@ -43,13 +46,31 @@ export default function NavBar() {
         </ul>
       </div>
       <div className="NavBar-left">
-        <Link to="/signUp" className="NavBar-links NavBar-signup">
-          <span>ثبت نام</span>
-          <LoginIcon
-            style={{ fontSize: "30px" }}
-            className="NavBar-login-icon"
-          />
-        </Link>
+        {isLogin ? (
+          <>
+            <div className="NavBar-logout-container">
+              <PersonIcon
+                style={{ fontSize: "40px" }}
+                className="NavBar-user-icon"
+              />
+              <div className="NavBar-logout">
+                <LogoutIcon
+                  style={{ fontSize: "20px" }}
+                  className="NavBar-logout-icon"
+                />
+                <span>خروج</span>
+              </div>
+            </div>
+          </>
+        ) : (
+          <Link to="/signUp" className="NavBar-links NavBar-signup-container">
+            <span>ثبت نام</span>
+            <LoginIcon
+              style={{ fontSize: "30px" }}
+              className="NavBar-login-icon"
+            />
+          </Link>
+        )}
       </div>
     </nav>
   );
