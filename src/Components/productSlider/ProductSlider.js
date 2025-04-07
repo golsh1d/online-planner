ea6d851d39ba93bed5ed68a42c1b641a19012d37
golshid <golsheedd@gmail.com>
2025-04-07 12:18:05 +0330

    add product slider title

---
+++ src/Components/productSlider/ProductSlider.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ProductSlider.css";
+import { Link } from "react-router-dom";
+// icon
+import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+
+export default function ProductSlider() {
+  return (
+    <div className="container">
+      <section className="ProductSlider-container ProductSlider-container-max-768 ProductSlider-container-min-768" dir="rtl">
+        <div className="ProductSlider-title ProductSlider-title-max-768 ProductSlider-title-min-768 ProductSlider-title-min-1024  ProductSlider-title-min-1280 ProductSlider-title-min-1920">
+          <span>محصولات ما</span>
+          <Link
+            to="/product"
+            className="ProductSlider-title-link ProductSlider-title-link-max-768 ProductSlider-title-link-min-768 ProductSlider-title-link-min-1024 ProductSlider-title-link-min-1280 ProductSlider-title-link-min-1920  "
+          >
+            <span>مشاهده</span>
+            <ChevronLeftRoundedIcon id="ProductSlider-chevron-icon" className="ProductSlider-chevron-icon-max-768 ProductSlider-chevron-icon-min-768 ProductSlider-chevron-icon-min-1024 ProductSlider-chevron-icon-min-1280 ProductSlider-chevron-icon-min-1920" />
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
 import HeroHeader from "../../Components/heroHeader/HeroHeader";
+import ProductSlider from "../../Components/productSlider/ProductSlider";
 
 export default function Home() {
   return (
     <>
       <HeroHeader />
+      <ProductSlider />
     </>
   );
 }
