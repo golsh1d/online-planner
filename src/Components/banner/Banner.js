62ab3cf52c49e7a7d1528eeb559491d6147bab99
golshid <golsheedd@gmail.com>
2025-04-08 18:14:43 +0330

    finish responsive banner

---
+++ src/Components/banner/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Banner.css";
+
+export default function Banner() {
+  return (
+    <div className="Banner-container Banner-container-max-width-768 Banner-container-min-width-768 ">
+      <img src="./Img/idea.png" alt="idea" />
+    </div>
+  );
+}
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import HeroHeader from "../../Components/heroHeader/HeroHeader";
 import ProductSlider from "../../Components/productSlider/ProductSlider";
+import Banner from "../../Components/banner/Banner";
 
 export default function Home() {
   return (
     <>
       <HeroHeader />
       <ProductSlider />
+      <Banner />
     </>
   );
 }
