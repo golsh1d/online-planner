acf1179d6d6445ec392d97546b16d3531e16e9ca
golshid <golsheedd@gmail.com>
2025-05-24 10:04:54 +0330

    create responsive sign up page

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,8 +1,31 @@
-import React from 'react'
-import "./SignUp.css"
+import React from "react";
+import "./SignUp.css";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="SignUp-container">
+      <div className="SignUp-wrapper SignUp-wrapper-max-768 SignUp-wrapper-min-768">
+        <div className="SignUp-title SignUp-title-max-768 SignUp-title-min-768">
+          ثبت نام
+        </div>
+        <div className="SignUp-input-wrapper">
+          <input
+            type="text"
+            className="SignUp-input SignUp-input-max-768 SignUp-input-min-768 SignUp-input-user"
+            placeholder="نام کاربری"
+          />
+        </div>
+        <div className="SignUp-input-wrapper">
+          <input
+            type="text"
+            className="SignUp-input SignUp-input-max-768 SignUp-input-min-768 SignUp-input-pass"
+            placeholder="رمز عبور"
+          />
+        </div>
+        <button className="SignUp-button SignUp-button-max-768 SignUp-button-min-768">
+          ثبت نام
+        </button>
+      </div>
+    </div>
+  );
 }
