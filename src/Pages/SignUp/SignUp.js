68dac7d7cab37eff09963da668b43eb600292ff9
golshid <golsheedd@gmail.com>
2025-05-25 11:12:24 +0330

    add clickBtn function in sign up component to send req to backend

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useState } from "react";
 import "./SignUp.css";
 
 export default function SignUp() {
+  const [userName, setUserName] = useState("");
+  const [passWord, setPassWord] = useState("");
+
+  function clickBtn() {
+    let userData = {
+      userName: userName,
+      passWord: passWord,
+    };
+
+    fetch("http://localhost:3000/users/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length > 0) {
+          console.log("found");
+        } else {
+          console.log("not found");
+        }
+      });
+  }
+
   return (
     <div className="SignUp-container">
       <div className="SignUp-wrapper SignUp-wrapper-max-768 SignUp-wrapper-min-768">
@@ -13,6 +40,8 @@ export default function SignUp() {
             type="text"
             className="SignUp-input SignUp-input-max-768 SignUp-input-min-768 SignUp-input-user"
             placeholder="نام کاربری"
+            value={userName}
+            onChange={(event) => setUserName(event.target.value)}
           />
         </div>
         <div className="SignUp-input-wrapper">
@@ -20,9 +49,14 @@ export default function SignUp() {
             type="text"
             className="SignUp-input SignUp-input-max-768 SignUp-input-min-768 SignUp-input-pass"
             placeholder="رمز عبور"
+            value={passWord}
+            onChange={(event) => setPassWord(event.target.value)}
           />
         </div>
-        <button className="SignUp-button SignUp-button-max-768 SignUp-button-min-768">
+        <button
+          className="SignUp-button SignUp-button-max-768 SignUp-button-min-768"
+          onClick={clickBtn}
+        >
           ثبت نام
         </button>
       </div>
