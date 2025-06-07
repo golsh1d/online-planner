64ac3f900d09e253d061b6250c6cd2567e54d3d8
golshid <golsheedd@gmail.com>
2025-06-07 14:20:05 +0330

    add some changes to publish demo version

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const basename =
-  process.env.NODE_ENV === "production" ? "/online-planner" : "/";
+// const basename =
+//   process.env.NODE_ENV === "production" ? "/online-planner" : "/";
 
 root.render(
-  <BrowserRouter basename={basename}>
+  <HashRouter >
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
